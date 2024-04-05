008951fea576ca39ef298faa052046dfedd523a9
godakid <godakid@outlook.com>
2024-04-05 21:34:26 +0800

    适配移动端

--- src/App.tsx
+++ src/App.tsx
@@ -23,29 +23,16 @@ import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkDirectiveRehype from 'remark-directive-rehype';
 import remarkDirective from 'remark-directive';
-import { unified } from 'unified';
 import { visit } from 'unist-util-visit';
 import { Node } from 'hast';
 import './markdown-preview/theme-default.less';
+import { PanZoom } from 'react-easy-panzoom';
 
 function App() {
   const [markdown, asyncMarkdown] = useState(Profile);
-  const processor = unified().use(remarkParse);
 
-  useEffect(() => {
-    (async () => {
-      const doc = processor.parse(markdown);
-    })();
-  }, [markdown]);
+  const cached = new Map();
 
-  async function save() {
-  }
-
-  let cached = new Map();
-
-  function onRenderer() {
-
-  }
 
   useEffect(() => {
     setTimeout(() => {
@@ -82,6 +69,8 @@ function App() {
         cx(`
     w-screen h-[calc(100vh-40px)]  overflow-y-auto overflow-x-hidden flex  preview 
     flex-row
+    lt-sm:h-fit
+    lt-sm:flex-col-reverse
     `)}
       >
         <MonacoEditor
@@ -99,91 +88,100 @@ function App() {
             'semanticHighlighting.enabled': true,
             contextmenu: false, // 启用上下文菜单
             cursorSmoothCaretAnimation: 'on',
+            automaticLayout: true,
           }}
           w-full
           h-full
-
+          className={cx(`
+            lt-sm:min-h-100vh z-max
+          `)}
           onChange={(markdown: string) => {
             asyncMarkdown(markdown);
           }}
         />
-
-        <Markdown
-          remarkPlugins={[
-            remarkParse,
-            remarkParserYaml,
-            remarkFrontmatter,
-            [function() {
-              return (ast) => {
-                const meta = ast.children
-                  .filter(({ type }: Node) => type === 'yaml' || type === 'toml')
-                  .map((item: Node) => {
-                    console.log(item);
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-expect-error
-                    return (item.data as unknown)?.parsedValue as Record<string, unknown>;
-                  }).reduce((pre: Record<string, unknown>, now: Record<string, unknown>) => {
-                    return {
-                      ...pre,
-                      ...(now ?? {}),
-                    };
-                  }, {});
-
-                Object.entries(meta).forEach(([key, value]) => {
-                  cached.set(key, value);
-                });
-              };
-            }],
-            remarkGemoji,
-            [remarkGfm, { singleTilde: false }],
-            [remarkDirective, {}],
-            remarkDirectiveRehype,
-            [remarkRehype, {
-              handlers: {},
-            }],
-            [
-              () => {
-                return (tree) => {
-                  visit(tree, (node) => {
-                    if (node.type === 'element') {
-                      node.properties = {
-                        ...node.properties,
-                        className: [...(node.properties.className ?? []), ...([`resume-theme-${cached.get('theme') ?? 'default'}`.replace(/\s*/g, '')])],
+        <PanZoom
+          boundaryRatioVertical={0.8}
+          enableBoundingBox
+          autoCenter
+          className={cx('z-1 overflow-x-hidden')}
+        >
+          <Markdown
+            remarkPlugins={[
+              remarkParse,
+              remarkParserYaml,
+              remarkFrontmatter,
+              [function() {
+                return (ast) => {
+                  const meta = ast.children
+                    .filter(({ type }: Node) => type === 'yaml' || type === 'toml')
+                    .map((item: Node) => {
+                      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                      // @ts-expect-error
+                      return (item.data as unknown)?.parsedValue as Record<string, unknown>;
+                    }).reduce((pre: Record<string, unknown>, now: Record<string, unknown>) => {
+                      return {
+                        ...pre,
+                        ...(now ?? {}),
                       };
-                    }
+                    }, {});
+
+                  Object.entries(meta).forEach(([key, value]) => {
+                    cached.set(key, value);
                   });
                 };
+              }],
+              remarkGemoji,
+              [remarkGfm, { singleTilde: false }],
+              [remarkDirective, {}],
+              remarkDirectiveRehype,
+              [remarkRehype, {
+                handlers: {},
+              }],
+              [
+                () => {
+                  return (tree) => {
+                    visit(tree, (node) => {
+                      if (node.type === 'element') {
+                        node.properties = {
+                          ...node.properties,
+                          className: [...(node.properties.className ?? []), ...([`resume-theme-${cached.get('theme') ?? 'default'}`.replace(/\s*/g, '')])],
+                        };
+                      }
+                    });
+                  };
+                },
+              ],
+            ]}
+            rehypePlugins={[]}
+
+            className={cx(
+              `w-full max-h-[calc(100vh-40px)] overflow-y-auto overflow-x-hidden 
+            lt-sm:h-fit
+            `,
+            )}
+            components={{
+              code(props) {
+                const { children, className, node, ...rest } = props;
+                const match = /language-(\w+)/.exec(className || '');
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                //@ts-expect-error
+                return match ? <SyntaxHighlighter
+                  {...rest}
+                  PreTag="div"
+                  children={String(children ?? '').replace(/\n$/, '')}
+                  language={match[1]}
+                  style={oneLight}
+                /> : <code {...rest} className={className}>
+                  {children ?? ''}
+                </code>;
               },
-            ],
-          ]}
-          rehypePlugins={[]}
 
-          className={cx(
-            `w-full max-h-[calc(100vh-40px)] overflow-y-auto overflow-x-hidden `,
-            css``,
-          )}
-          components={{
-            code(props) {
-              const { children, className, node, ...rest } = props;
-              const match = /language-(\w+)/.exec(className || '');
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              //@ts-expect-error
-              return match ? <SyntaxHighlighter
-                {...rest}
-                PreTag="div"
-                children={String(children ?? '').replace(/\n$/, '')}
-                language={match[1]}
-                style={oneLight}
-              /> : <code {...rest} className={className}>
-                {children ?? ''}
-              </code>;
-            },
+            }}
 
-          }}
-
-        >
-          {markdown}
-        </Markdown>
+          >
+            {markdown}
+          </Markdown>
+        </PanZoom>
       </main>
     </>
   );
--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,12 @@
 // uno.config.js
 import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss';
+import presetWind from '@unocss/preset-wind';
 
 export default defineConfig({
   presets: [
     presetUno(), // 添加 UnoCSS 的默认样式预设
     presetAttributify(),
     presetIcons(),
+    presetWind(),
   ],
 });
